b591ae7f47f32e639b2752939cf511bde6a47e4d
davudk <14043994+davudk@users.noreply.github.com>
2021-12-01 09:57:04 -0500

    Updated config

--- src/components/AsyncRender.tsx
+++ src/components/AsyncRender.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import { AsyncController } from "../utils";
 import { AsyncRenderContext, AsyncRenderState } from "./AsyncRenderState";
 
@@ -8,7 +8,6 @@ export interface AsyncRenderProps<TResult = any, TParams = any> {
 }
 
 export function AsyncRender<TResult = any, TParams = any>(props: AsyncRenderProps<TResult, TParams>) {
-
     return (
         <AsyncRenderContext.Provider value={props.control}>
             {props.children}
--- src/components/AsyncRenderState.tsx
+++ src/components/AsyncRenderState.tsx
@@ -1,12 +1,13 @@
-import { createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext } from "react";
 import { AsyncController } from "../utils";
 
 export const AsyncRenderContext = createContext<AsyncController | null>(null);
 
-export interface AsyncRenderStateProps {
+export interface AsyncRenderStateProps<TResult = any, TParams = any> {
     children?: ReactNode;
+    control?: AsyncController<TResult, TParams>;
 
-    mode: 'any' | 'all';
+    mode?: 'any' | 'all';
 
     result?: boolean;
     prevResult?: boolean;
@@ -15,10 +16,11 @@ export interface AsyncRenderStateProps {
     empty?: boolean;
 }
 
-export function AsyncRenderState(props: AsyncRenderStateProps) {
+export function AsyncRenderState<TResult = any, TParams = any>(props: AsyncRenderStateProps<TResult, TParams>) {
     const { mode } = props;
 
-    const controller = useContext(AsyncRenderContext);
+    const ctx = useContext(AsyncRenderContext);
+    const controller = props.control ?? ctx;
     if (!controller) return null;
 
     const conditions = [
@@ -38,7 +40,7 @@ export function AsyncRenderState(props: AsyncRenderStateProps) {
         throw new Error('Unknown mode specified: ' + mode)
     }
 
-    return invalid || props.children;
+    return invalid ? null : <>{props.children}</>;
 
     function check(condition: boolean | undefined, valuePresent: boolean) {
         if (condition === undefined) return undefined;
