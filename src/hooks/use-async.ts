ee238d5fde2bea3fe762295daa62cab69e5b99d1
davudk <14043994+davudk@users.noreply.github.com>
2022-01-12 11:42:38 -0500

    Update use-async.ts

--- src/hooks/use-async.ts
+++ src/hooks/use-async.ts
@@ -1,19 +1,23 @@
 import { useRef } from 'react';
 import { AsyncController, AsyncControllerOptions } from '../utils';
 import { useForceRender } from './use-force-render';
+import { useIsMounted } from './use-is-mounted';
 
 export interface AsyncOptions<TResult = any, TParams = any> extends AsyncControllerOptions<TResult, TParams> {
 }
 
 export function useAsync<TResult = any, TParams = any>(options: AsyncOptions<TResult, TParams>): AsyncController<TResult, TParams> {
     const controllerRef = useRef<AsyncController<TResult, TParams>>();
+    const isMounted = useIsMounted();
     const forceRender = useForceRender();
 
     if (!controllerRef.current) {
         controllerRef.current = new AsyncController<TResult, TParams>({
             ...options,
             onMutate: () => {
-                forceRender();
+                if (isMounted()) {
+                    forceRender();
+                }
                 options?.onMutate?.();
             }
         });
